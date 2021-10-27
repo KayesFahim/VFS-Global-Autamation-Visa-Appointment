4cecdfd40b8720c30e52feb3fe615037d295dc0c
Kayes Fahim <kayes.fuad@northsouth.edu>
2021-10-27 22:27:00 +0600

    Ok

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+from io import BytesIO
 import time
 
 driver = webdriver.Chrome('chromedriver.exe')
-driver.get("https://visa.vfsglobal.com/ind/en/deu/login")
-wait = WebDriverWait(driver, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
-element = wait.until(EC.element_to_be_clickable((By.XPATH, "//div")))
+driver.maximize_window()
+time.sleep(2)
+driver.get("https://row1.vfsglobal.com/GlobalAppointment/Account/RegisteredLogin")
+
+#Login
+
+driver.find_element_by_id('EmailId').send_keys('kayes.fuad@northsouth.edu') #Email
+driver.find_element_by_id('Password').send_keys('@Kayes321') #password
+
+#Take screenshot
+
+image = driver.find_element_by_id('CaptchaImage').screenshot_as_png
+im = Image.open(BytesIO(image))  # uses PIL library to open image in memory
+im.save('captch.png')
+
+#Load Image
+
+LoadImage = Image.open("captch.png")
+
+# construct the argument parser and parse the arguments}
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+args = vars(ap.parse_args())
+
+LoadImage = Image.open(args["captch.png"])
+# use Tesseract to OCR the image
+text = pytesseract.image_to_string(LoadImage)
+print(text)
+
+time.sleep(5)
+#Captch Solver
+driver.find_element_by_id('CaptchaInputText').send_keys("xyzzz") #captcha
 
 
-
-driver.find_element(By.id("mat-input-0")).send_keys("Abc@gmail.com")
 
