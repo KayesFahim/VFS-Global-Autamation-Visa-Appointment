d52ebf49294d9b7a92409f986a2c91a0f5810759
Md Kayes Hossan Fuad <kayes.fuad@northsouth.edu>
2021-11-04 18:33:56 +0600

    ok

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common import by
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -16,11 +17,11 @@ String_Url ='https://row1.vfsglobal.com/GlobalAppointment/Account/RegisteredLogi
 driver.get(String_Url)
 
 #Login
-driver.find_element_by_id('EmailId').send_keys('a1@flyfarint.org') #Email
-driver.find_element_by_id('Password').send_keys('@Kayes321') #password
+driver.find_element(by=By.ID, value='EmailId').send_keys('a1@flyfarint.org') #Email
+driver.find_element(by=By.ID, value='Password').send_keys('@Kayes321') #password
 
 #Take screenshot
-image = driver.find_element_by_id('CaptchaImage').screenshot_as_png
+image = driver.find_element(by=By.ID, value='CaptchaImage').screenshot_as_png
 im = Image.open(BytesIO(image)).convert('L')  # uses PIL library to open image in memory
 im.save('captcha.png')
 
@@ -31,7 +32,7 @@ captchtext = ' '.join(map(str, output))
 finalCaptchText = re.escape(captchtext.upper())
 
 #Captch Solver
-driver.find_element_by_id('CaptchaInputText').send_keys("") #captcha
+driver.find_element(by=By.ID, value='CaptchaInputText').send_keys("") #captcha
 
 time.sleep(8)
 driver.find_element_by_xpath('//*[@id="ApplicantListForm"]/div[4]').click() #Click Login Button
@@ -40,28 +41,29 @@ driver.find_element_by_xpath('//*[@id="ApplicantListForm"]/div[4]').click() #Cli
 driver.find_element_by_xpath('//*[@id="Accordion1"]/div/div[2]/div/ul/li[1]').click()
 
 #Select Location
-ddelement= Select(driver.find_element_by_id('LocationId'))
+ddelement= Select(driver.find_element(by=By.ID, value='LocationId'))
 ddelement.select_by_index(6)
 
 #Select Visa Category
-ddelement= Select(driver.find_element_by_id('VisaCategoryId'))
+ddelement= Select(driver.find_element(by=By.ID, value='VisaCategoryId'))
 ddelement.select_by_index(1)
 
-driver.find_element_by_id('btnContinue').click() #Visa Page button click
+driver.find_element(by=By.ID, value='btnContinue').click() #Visa Page button click
 
 
-for i in range(0, 5):
+for i in range(0, 1):
     #Add Customer
     driver.find_element_by_link_text('Add Customer').click()   
-    driver.find_element_by_id('PassportNumber').send_keys("BN"+str(random.randint(0, 9999999))+str(i+3))
-    driver.find_element_by_id('DateOfBirth').send_keys("11011998")
-    driver.find_element_by_id('PassportExpiryDate').send_keys("01012025")
-    ddelement= Select(driver.find_element_by_id('NationalityId'))
+    driver.find_element(by=By.ID, value='PassportNumber').send_keys("BN"+str(random.randint(0, 9999999))+str(i+3))
+    driver.find_element(by=By.ID, value='DateOfBirth').send_keys("11011998")
+    driver.find_element(by=By.ID, value='PassportExpiryDate').send_keys("01012025")
+    ddelement= Select(driver.find_element(by=By.ID, value='NationalityId'))
     ddelement.select_by_visible_text('BANGLADESH')
-    driver.find_element_by_id('Mobile').clear()
-    driver.find_element_by_id('Mobile').send_keys('1685370455')
+    driver.find_element(by=By.ID, value='Mobile').clear()
+    driver.find_element(by=By.ID, value='Mobile').send_keys('1685370455')
     ddelement= Select(driver.find_element_by_id('GenderId'))
     ddelement.select_by_visible_text('Male')
+
     #Button Click
     driver.find_element(By.ID, 'submitbuttonId').click()
     accept = driver.switch_to.alert
@@ -72,10 +74,10 @@ for i in range(0, 5):
 #OTP
 driver.find_element(By.XPATH, '//*[@id="ApplicantListForm"]/div[2]').click() #OTP send
 
-time.sleep(30)
+time.sleep(60)
 #OTP reading
-host = 'imap.gmail.com'
-username = 'mrfawbd@gmail.com'
+host = 'flyfarint.org'
+username = 'a1@flyfarint.org'
 password = '@Kayes321'
 
 
@@ -112,19 +114,28 @@ if __name__ == "__main__":
 
 time.sleep(5)    
 #OTP
-driver.find_element_by_id('OTPe').send_keys(finalOTP) #captcha
+driver.find_element(by=By.ID, value='OTPe').send_keys(finalOTP) #captcha
 
-driver.find_element_by_id('txtsub').click()
+driver.find_element(by=By.ID, value='txtsub').click()
 
 
-driver.find_element_by_id('btnContinueService').click()
-
-#driver.find_element_by_css_selector("div[style='background-color: rgb(188, 237, 145); cursor: pointer;']")
-#driver.find_element_by_css_selector("input[type='radio'][value='xUc2m4DGouH7OxZSdHnSfqXhFube1hrRw4luyX2FSZA=']").click()
+driver.find_element(by=By.ID, value='btnContinueService').click()
 
 time.sleep(10)
-driver.find_element_by_id('ReachVFS').click()
-driver.find_element_by_id('IAgree').click()
+driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/div[3]/form/table/tbody/tr/td[1]/div/div/div/table/tbody/tr[1]/td[4]' ).click()
+driver.find_element(by=By.TAG_NAME, value='selectedTimeBand').click()
+
+
+#Button Click
+driver.find_element(By.ID, 'btnConfirm').click()
+accept = driver.switch_to.alert
+accept.accept()
+
+
+
+time.sleep(10)
+driver.find_element(by=By.ID, value='ReachVFS').click()
+driver.find_element(by=By.ID, value='IAgree').click()
 
 driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
 
